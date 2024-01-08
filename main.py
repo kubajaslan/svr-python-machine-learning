169048b9303c1f528483d9c01d431ee7c0a21092
jakub.jaslan <jakub.jaslan@sollers.eu>
2024-01-08 11:42:05 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+print(x)
